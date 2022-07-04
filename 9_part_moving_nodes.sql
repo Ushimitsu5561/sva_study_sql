f5f419758dcac51a131eedb8c47410ddcd2a4545
Ushimitsu5561 <78661312+Ushimitsu5561@users.noreply.github.com>
2022-07-04 10:16:08 +0300

    add last changes

--- 9_part_moving_nodes.sql
+++ 9_part_moving_nodes.sql
@@ -48,4 +48,29 @@ somedata as ( select level, left_key, right_key from my_tree where id=10 ), some
 select * from my_tree
 where my_tree.level = ( select level from somedata ) or level = ( select level from somedata2 )
 */
+
+-- Выбор ключей перемещаемого узла
+-- 1. Ключи и уровень перемещаемого узла (переместим узел с id=10);
+asd integer := 5;
+with
+moved_node as (select level, left_key, right_key from my_tree where id=10),
+-- 2. Уровень нового родительского узла (переместим в узел с id=9)
+nparent_node as (select level from my_tree where id=9),
+-- 3. Правый ключ узла за который мы вставляем узел (ветку)
+prev_key as (select right_key=right_key-1 as right_key from my_tree where id=9);
+-- 4. Определяем смещения:
+--declare @skew_level integer, @skew_tree integer, @id_edit integer
+skew_level integer := (select (nparent_node.level-moved_node.level+1) from nparent_node, moved_node);
+skew_tree integer := (select (prev_key.right_key-moved_node.left.key+1) from prev_key, moved_node);
+-- Выбираем все узлы перемещаемой ветки:
+id_edit integer := (select id from my_tree where left_key >= (select left_key from moved_node) and right_key <= (select right_key from moved_node));
+
+
+-- Так как при в условиях не участвуют ключи кроме изменяемых, то порядок действий не имеет значения.
+update my_tree set right_key = right_key + @skew_tree WHERE right_key < (select left_key from moved_node) AND right_key > (select right_key from prev_key);
+update my_tree set left_key = left_key + $skew_tree WHERE left_key < (select left_key from moved_node) AND left_key > (select right_key from prev_key)
+
+-- Теперь можно переместить ветку:
+update my_tree set left_key = left_key + $skew_edit, right_key = right_key + $skew_edit, level = level + $skew_level where id in (@id_edit) 
+
 SELECT concat(lpad('', level*2, '- '), name) AS список_узлов FROM my_tree ORDER BY left_key
